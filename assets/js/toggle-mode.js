7a22a887744afc44bc9bf1729aa0b936f9dcfe66
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-08-10 03:11:56 +0800

    delete console.log

--- assets/js/toggle-mode.js
+++ assets/js/toggle-mode.js
@@ -5,20 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
     let icon = modeLogo.querySelector('i');
     let stickyIcon = stickyModeLogo.querySelector('i');
 
-    console.log('current theme: ', localStorage.getItem('theme'));
     if (localStorage.getItem('theme')) {
         appElement.classList.remove('theme--light', 'theme--dark');
-        console.log('if test: ', localStorage.getItem('theme'));
         appElement.classList.add(localStorage.getItem('theme'));
 
         if (localStorage.getItem('theme') == 'theme--light') {
-            console.log('test 1');
             icon.id = 'Light-mode';
             stickyIcon.id = 'Light-mode';
             icon.className = 'bi bi-moon-stars-fill';
             stickyIcon.className = 'bi bi-moon-stars-fill';
         } else if (localStorage.getItem('theme') == 'theme--dark') {
-            console.log('test 2');
             icon.id = 'Dark-mode';
             stickyIcon.id = 'Dark-mode';
             icon.className = 'bi bi-sun-fill';
@@ -37,8 +33,6 @@ document.addEventListener("DOMContentLoaded", function() {
             appElement.classList.add('theme--dark');
     
             localStorage.setItem('theme', 'theme--dark');
-            console.log('toggle to dark: ', localStorage.getItem('theme'));
-            // console.log('toggle to dark');
         } else if (icon.id == 'Dark-mode' && stickyIcon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
             icon.id = 'Light-mode';
             stickyIcon.id = 'Light-mode';
@@ -49,8 +43,6 @@ document.addEventListener("DOMContentLoaded", function() {
             appElement.classList.add('theme--light');
     
             localStorage.setItem('theme', 'theme--light');
-            console.log('toggle to light: ', localStorage.getItem('theme'));
-            // console.log('toggle to light');
         }
     })
     
@@ -65,8 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
             appElement.classList.add('theme--dark');
     
             localStorage.setItem('theme', 'theme--dark');
-            console.log('toggle to dark: ', localStorage.getItem('theme'));
-            // console.log('toggle to dark');
         } else if (icon.id == 'Dark-mode' && stickyIcon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
             icon.id = 'Light-mode';
             stickyIcon.id = 'Light-mode';
@@ -77,36 +67,6 @@ document.addEventListener("DOMContentLoaded", function() {
             appElement.classList.add('theme--light');
     
             localStorage.setItem('theme', 'theme--light');
-            console.log('toggle to light: ', localStorage.getItem('theme'));
-            // console.log('toggle to light');
         }
     })
 });
-
-// function toggleMode() {        
-//     if (icon.id == 'Light-mode' && stickyIcon.id == 'Light-mode' && appElement.classList.contains('theme--light')) {
-//         icon.id = 'Dark-mode';
-//         stickyIcon.id = 'Dark-mode';
-//         icon.className = 'bi bi-sun-fill';
-//         stickyIcon.className = 'bi bi-sun-fill';
-
-//         appElement.classList.remove('theme--light');
-//         appElement.classList.add('theme--dark');
-
-//         localStorage.setItem('theme', 'theme--dark');
-//         console.log('toggle to dark: ', localStorage.getItem('theme'));
-//         // console.log('toggle to dark');
-//     } else if (icon.id == 'Dark-mode' && stickyIcon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
-//         icon.id = 'Light-mode';
-//         stickyIcon.id = 'Light-mode';
-//         icon.className = 'bi bi-moon-stars-fill';
-//         stickyIcon.className = 'bi bi-moon-stars-fill';
-
-//         appElement.classList.remove('theme--dark');
-//         appElement.classList.add('theme--light');
-
-//         localStorage.setItem('theme', 'theme--light');
-//         console.log('toggle to light: ', localStorage.getItem('theme'));
-//         // console.log('toggle to light');
-//     }
-// }
