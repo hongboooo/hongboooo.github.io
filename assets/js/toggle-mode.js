2b05e65aafef66d4f21fbca31455bf29989125aa
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-08-10 03:09:02 +0800

    solve theme reload problem

--- assets/js/toggle-mode.js
+++ assets/js/toggle-mode.js
@@ -1,26 +1,112 @@
-window.onload = function() {toggleMode();};
-
-function toggleMode() {
-    let modeLogo = document.getElementById('mode-toggler');
-    let icon = modeLogo.querySelector('i');
+document.addEventListener("DOMContentLoaded", function() {
     let appElement = document.getElementById('app');
-
-    if (icon.id == 'Light-mode' && appElement.classList.contains('theme--light')) {
-        icon.id = 'Dark-mode';
-        icon.className = 'bi bi-sun-fill';
-        appElement.classList.remove('theme--light');
-        appElement.classList.add('theme--dark');
-    } else if (icon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
-        icon.id = 'Light-mode';
-        icon.className = 'bi bi-moon-stars-fill';
-        appElement.classList.remove('theme--dark');
-        appElement.classList.add('theme--light');
-    }
-    
+    let modeLogo = document.getElementById('mode-toggler');
     let stickyModeLogo = document.getElementById('sticky-mode-toggler');
-    if (stickyModeLogo.innerHTML == '<i id="Light-mode" class="bi bi-moon-stars-fill"></i>') {
-        stickyModeLogo.innerHTML = '<i id="Dark-mode" class="bi bi-sun-fill"></i>';
-    } else {
-        stickyModeLogo.innerHTML = '<i id="Light-mode" class="bi bi-moon-stars-fill"></i>';
+    let icon = modeLogo.querySelector('i');
+    let stickyIcon = stickyModeLogo.querySelector('i');
+
+    console.log('current theme: ', localStorage.getItem('theme'));
+    if (localStorage.getItem('theme')) {
+        appElement.classList.remove('theme--light', 'theme--dark');
+        console.log('if test: ', localStorage.getItem('theme'));
+        appElement.classList.add(localStorage.getItem('theme'));
+
+        if (localStorage.getItem('theme') == 'theme--light') {
+            console.log('test 1');
+            icon.id = 'Light-mode';
+            stickyIcon.id = 'Light-mode';
+            icon.className = 'bi bi-moon-stars-fill';
+            stickyIcon.className = 'bi bi-moon-stars-fill';
+        } else if (localStorage.getItem('theme') == 'theme--dark') {
+            console.log('test 2');
+            icon.id = 'Dark-mode';
+            stickyIcon.id = 'Dark-mode';
+            icon.className = 'bi bi-sun-fill';
+            stickyIcon.className = 'bi bi-sun-fill';
+        }
     }
-}
+
+    modeLogo.addEventListener('click', function() {
+        if (icon.id == 'Light-mode' && stickyIcon.id == 'Light-mode' && appElement.classList.contains('theme--light')) {
+            icon.id = 'Dark-mode';
+            stickyIcon.id = 'Dark-mode';
+            icon.className = 'bi bi-sun-fill';
+            stickyIcon.className = 'bi bi-sun-fill';
+    
+            appElement.classList.remove('theme--light');
+            appElement.classList.add('theme--dark');
+    
+            localStorage.setItem('theme', 'theme--dark');
+            console.log('toggle to dark: ', localStorage.getItem('theme'));
+            // console.log('toggle to dark');
+        } else if (icon.id == 'Dark-mode' && stickyIcon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
+            icon.id = 'Light-mode';
+            stickyIcon.id = 'Light-mode';
+            icon.className = 'bi bi-moon-stars-fill';
+            stickyIcon.className = 'bi bi-moon-stars-fill';
+    
+            appElement.classList.remove('theme--dark');
+            appElement.classList.add('theme--light');
+    
+            localStorage.setItem('theme', 'theme--light');
+            console.log('toggle to light: ', localStorage.getItem('theme'));
+            // console.log('toggle to light');
+        }
+    })
+    
+    stickyModeLogo.addEventListener('click', function() {
+        if (icon.id == 'Light-mode' && stickyIcon.id == 'Light-mode' && appElement.classList.contains('theme--light')) {
+            icon.id = 'Dark-mode';
+            stickyIcon.id = 'Dark-mode';
+            icon.className = 'bi bi-sun-fill';
+            stickyIcon.className = 'bi bi-sun-fill';
+    
+            appElement.classList.remove('theme--light');
+            appElement.classList.add('theme--dark');
+    
+            localStorage.setItem('theme', 'theme--dark');
+            console.log('toggle to dark: ', localStorage.getItem('theme'));
+            // console.log('toggle to dark');
+        } else if (icon.id == 'Dark-mode' && stickyIcon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
+            icon.id = 'Light-mode';
+            stickyIcon.id = 'Light-mode';
+            icon.className = 'bi bi-moon-stars-fill';
+            stickyIcon.className = 'bi bi-moon-stars-fill';
+    
+            appElement.classList.remove('theme--dark');
+            appElement.classList.add('theme--light');
+    
+            localStorage.setItem('theme', 'theme--light');
+            console.log('toggle to light: ', localStorage.getItem('theme'));
+            // console.log('toggle to light');
+        }
+    })
+});
+
+// function toggleMode() {        
+//     if (icon.id == 'Light-mode' && stickyIcon.id == 'Light-mode' && appElement.classList.contains('theme--light')) {
+//         icon.id = 'Dark-mode';
+//         stickyIcon.id = 'Dark-mode';
+//         icon.className = 'bi bi-sun-fill';
+//         stickyIcon.className = 'bi bi-sun-fill';
+
+//         appElement.classList.remove('theme--light');
+//         appElement.classList.add('theme--dark');
+
+//         localStorage.setItem('theme', 'theme--dark');
+//         console.log('toggle to dark: ', localStorage.getItem('theme'));
+//         // console.log('toggle to dark');
+//     } else if (icon.id == 'Dark-mode' && stickyIcon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
+//         icon.id = 'Light-mode';
+//         stickyIcon.id = 'Light-mode';
+//         icon.className = 'bi bi-moon-stars-fill';
+//         stickyIcon.className = 'bi bi-moon-stars-fill';
+
+//         appElement.classList.remove('theme--dark');
+//         appElement.classList.add('theme--light');
+
+//         localStorage.setItem('theme', 'theme--light');
+//         console.log('toggle to light: ', localStorage.getItem('theme'));
+//         // console.log('toggle to light');
+//     }
+// }
