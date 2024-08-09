c2b0ffb9514d0f0ab0851289eeddae79ba3aac82
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-08-10 00:26:15 +0800

    enable theme toggle

--- assets/js/toggle-mode.js
+++ assets/js/toggle-mode.js
@@ -1,11 +1,20 @@
-window.onload = function() {toggleMode()};
+window.onload = function() {toggleMode();};
 
 function toggleMode() {
     let modeLogo = document.getElementById('mode-toggler');
-    if (modeLogo.innerHTML == '<i id="Light-mode" class="bi bi-moon-stars-fill"></i>') {
-        modeLogo.innerHTML = '<i id="Dark-mode" class="bi bi-sun-fill"></i>';
-    } else {
-        modeLogo.innerHTML = '<i id="Light-mode" class="bi bi-moon-stars-fill"></i>';
+    let icon = modeLogo.querySelector('i');
+    let appElement = document.getElementById('app');
+
+    if (icon.id == 'Light-mode' && appElement.classList.contains('theme--light')) {
+        icon.id = 'Dark-mode';
+        icon.className = 'bi bi-sun-fill';
+        appElement.classList.remove('theme--light');
+        appElement.classList.add('theme--dark');
+    } else if (icon.id == 'Dark-mode' && appElement.classList.contains('theme--dark')) {
+        icon.id = 'Light-mode';
+        icon.className = 'bi bi-moon-stars-fill';
+        appElement.classList.remove('theme--dark');
+        appElement.classList.add('theme--light');
     }
     
     let stickyModeLogo = document.getElementById('sticky-mode-toggler');
