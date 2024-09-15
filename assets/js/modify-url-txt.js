3c2dea022e530c88d02324fc801e1bac79deb65e
hongboooo <hongbozhang@zju.edu.cn>
2024-09-15 13:24:15 +0800

    update email

--- assets/js/modify-url-txt.js
+++ assets/js/modify-url-txt.js
@@ -5,7 +5,7 @@ let brkp = 860;
 function modTxt() {
     let width = window.innerWidth;
     if (width >= brkp) {
-        document.getElementById("email").innerHTML = '<i class="fas fa-fw fa-envelope icon-pad-right" aria-hidden="true"></i>' + "zy_cheng@zju.edu.cn";
+        document.getElementById("email").innerHTML = '<i class="fas fa-fw fa-envelope icon-pad-right" aria-hidden="true"></i>' + "hongbozhang@zju.edu.cn";
         document.getElementById("github").innerHTML = '<i class="fab fa-fw fa-github icon-pad-right" aria-hidden="true"></i>' + "Juniper1106";
         document.getElementById("twitter").innerHTML = '<i class="fab fa-fw fa-x-twitter icon-pad-right" aria-hidden="true"></i>' + "zyCHENG_ELF";
     } else {
